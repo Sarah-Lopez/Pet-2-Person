508c891d8815057505222bbd88edd7317389f985
Marco Angel <angel2367716@gmail.com>
2019-10-15 19:01:08 -0700

    included routes for navbar on app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Dogs from "./pages/Dogs";
 import Cats from "./pages/Cats";
+import Banyard from "./pages/Banyard";
+import Birds from "./pages/Birds";
+import Horses from "./pages/Horses";
+import Rabbits from "./pages/Rabbits";
+import Scalesfinsother from "./pages/Scalesfinsother";
+import Smallnfurry from "./pages/Smallnfurry";
+
+
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -18,6 +26,12 @@ function App() {
             <Route exact path="/about" component={About} />
             <Route exact path="/dogs" component={Dogs} />
             <Route exact path="/cats" component={Cats} />
+            <Route exact path="/banyard" component={Banyard} />
+            <Route exact path="/birds" component={Birds} />
+            <Route exact path="/horses" component={Horses} />
+            <Route exact path="/rabbits" component={Rabbits} />
+            <Route exact path="/scalesfinsother" component={Scalesfinsother} />
+            <Route exact path="/smallnfurry" component={Smallnfurry} />
           </Wrapper>
         </Switch>
         <Footer />
