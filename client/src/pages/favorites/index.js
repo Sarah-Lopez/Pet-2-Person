7c8bfbfe0e31e72bb0ba7095866d9e2bb546592f
Sarah Lopez <Sarahanne.pez@gmail>
2019-10-26 00:59:30 -0700

    updating sample pets

--- client/src/pages/favorites/index.js
+++ client/src/pages/favorites/index.js
@@ -4,128 +4,94 @@ import Container from "../../components/container";
 import Row from "../../components/row";
 import Col from "../../components/col";
 import Hero from "../../components/hero";
+import PetRow, { myFavoritePets } from "../../components/list/index";
 // import API from "../../../utils/api";
 // import Card from "../../../components/Card/Card";
 // import Wrapper from "../../../components/Wrapper";
 // import Form from "../../../components/form";
 
-
-
 // *************************** Consult Chaa'llisa on this page***********************************
 
-
-// import React, { Component } from "react";
-// import PetRow, { myFavoritePets } from "../../components/list/index";
-
 class Favorite extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-  
-//   componentDidMount() {
-//     if (myFavoritePets.length > 0) {
-//       const petRows = [];
-//       const data = [{
-//         name: "Pupper",
-//         type: "Dog",
-//         location: "Miami, Fl",
-//         description: "A good boi",
-//         id: 1
-//       },
-//       {
-//         name: "Catter",
-//         description: "It's a cat, what can I say.",
-//         type: "Cat",
-//         location: "Raleigh, NC",
-//         id: 2
-//       }]
-//       for (var i = 0; i < data.length; i++) {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    if (myFavoritePets.length > 0) {
+      const petRows = [];
+      const data = [{
+        name: "Walter White",
+        type: "Dog",
+        location: "Albuquerque, NM",
+        description: "He is the danger. Usually very mild-mannered, but too smart for his own good.",
+        id: 1
+      },
+      {
+        name: "Bacon",
+        description: "A really crispy purr machine. This guy needs a home that cooks bacon frequently, as he has developed a taste for it that we can no longer accommodate. Not good with kids.",
+        type: "Cat",
+        location: "Two Egg, FL",
+        id: 2
+      }]
+      for (var i = 0; i < data.length; i++) {
         // fetch(myFavoritePets[i])
         // .then(res => res.json())
         // .then(data => {
-
-          // const petRow = (
-          //   <PetRow
-          //   key = {
-          //     data.id
-          //   }
-          //   pet={data[i]}
-          //   favornot
-          //   />
-          //   );
-          //   petRows.push(petRow);
-          //   this.setState({ rows: petRows });
-          // });
-    //     }
-    //   }
-    // }
-    
-
-    // render() {
-      // return (
-      //   <div className="Favorite">
-        // {/* {this.state.rows ? (
-        //   this.state.rows
-        //   ) : (
-        //     <h1>
-        //     {
-        //       "You can view your favorite pets here. If you open new session you're favorite pets will delete."
-        //     }
-        //   </h1>
-        // )} */}
-
-
-        render() {
-          return (
-            <div>
-            <Hero backgroundImage="https://i.imgur.com/aHowLUJ.jpg">
-                <h1>Favorites</h1>
-            </Hero>
-  
-            <Container style={{ marginTop: 30 }}>
-             <Row>
-             <Col size="md-8">
-             Feature comming soon!
-             </Col>
-             <Col size="md-4">
-             
-            
-             </Col>
-         </Row>
-         <Row>
-                 <Col size="md-8">
-
-                 </Col>
-              <Col size="md-4">
-                 
-                 </Col>
-        </Row>
-  
-             </Container>
-         </div>       
-          );
-       }
+        const petRow = (
+          <PetRow
+            key={
+              data.id
+            }
+            pet={data[i]}
+            favornot
+          />
+        );
+        petRows.push(petRow);
+        this.setState({ rows: petRows });
+      }
+    }
   }
-  
 
+  render() {
+    return (
+      <div>
+        <Hero backgroundImage="https://i.imgur.com/aHowLUJ.jpg">
+          <h1>Favorites</h1>
+        </Hero>
 
-//     render() {
-//       return (
-//         <div className="Favorites">
-//         {this.state.rows ? (
-//           this.state.rows
-//           ) : (
-//             <h1>
-//             {
-//               "You can view your favorite pets here. If you open a new session your favorite pets will delete."
-//             }
-//           </h1>
-//         )} */}
-//       </div>
-//     );
-//   }
-// }
+        <Container style={{ marginTop: 10 }}>
+          <Row>
+            <Col size="md-12">
+              Feature comming soon!
+             </Col>
+            <Col size="md-12">
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-10">
+              <div className="Favorites">
+                {this.state.rows ? (
+                  this.state.rows
+                ) : (
+                    <h1>
+                      {
+                        "You can view your favorite pets here. If you open a new session your favorite pets will delete."
+                      }
+                    </h1>
+                  )}
+              </div>
+            </Col>
+            <Col size="md-4">
 
+            </Col>
+          </Row>
+
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Favorite;
